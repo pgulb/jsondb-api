c1f20208984e4f2d3cca2b3b6c7073e62905fc86
pgul <trololo.rpgpg@gmail.com>
2023-12-16 21:40:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# github.com/pgulb/jsondb-api  
+  
+Example REST API dome using github.com/pgulb/jsondb and gin  
+  
+endpoints:  
+https://jsondb.toadres.pl/health  
+https://jsondb.toadres.pl/values  
+https://jsondb.toadres.pl/value/{value}  
---
+++ api/api.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/Valgard/godotenv"
+	"github.com/gin-gonic/gin"
+	"github.com/pgulb/jsondb/db"
+	"github.com/pgulb/jsondb/structures"
+)
+
+func logInfo(c *gin.Context) {
+	log.Printf("%s %s\n", c.Request.RequestURI, c.Request.RemoteAddr)
+}
+
+func main() {
+	timeout := 5
+	input := make(chan structures.Request)
+	output := make(chan structures.Response)
+	initialOutput := make(chan structures.Response)
+	cmdArgs := os.Args[1:]
+
+	log.Println("starting jsondb goroutine")
+	go db.Listen(cmdArgs, input, output, initialOutput)
+
+	resp, err := db.HandleOutput(initialOutput, timeout)
+	for _, v := range resp {
+		log.Println(v)
+	}
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	input <- structures.Request{
+		KeyFamily: "healthcheck",
+		Key:       "h",
+		Value:     "OK",
+		Action:    "set",
+	}
+	resp, err = db.HandleOutput(output, timeout)
+	for _, v := range resp {
+		log.Println(v)
+	}
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	dotenv := godotenv.New()
+	if err := dotenv.Load(".env"); err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	r := gin.Default()
+	a := r.Group("/", gin.BasicAuth(
+		gin.Accounts{
+			os.Getenv("API_USER"): os.Getenv("API_PASS"),
+		},
+	))
+
+	r.GET("/health", func(c *gin.Context) {
+		input <- structures.Request{
+			KeyFamily: "healthcheck",
+			Key:       "h",
+			Action:    "get",
+		}
+		resp, err := db.HandleOutput(output, timeout)
+		if err != nil {
+			log.Fatal(err)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+		} else {
+			c.Data(http.StatusOK, gin.MIMEJSON, []byte(resp[0]))
+		}
+		logInfo(c)
+	})
+
+	r.GET("/values", func(c *gin.Context) {
+		input <- structures.Request{
+			KeyFamily: "ram_usage",
+			Action:    "listkeys",
+		}
+		resp, err := db.HandleOutput(output, timeout)
+		if err != nil {
+			log.Fatal(err)
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+		} else {
+			c.Data(http.StatusOK, gin.MIMEJSON, []byte(resp[0]))
+		}
+		logInfo(c)
+	})
+
+	r.GET("/value/:value", func(c *gin.Context) {
+		value := c.Params.ByName("value")
+		if value == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "empty value was given",
+			})
+		} else {
+			input <- structures.Request{
+				Key:       value,
+				KeyFamily: "ram_usage",
+				Action:    "get",
+			}
+			resp, err := db.HandleOutput(output, timeout)
+			if err != nil {
+				log.Fatal(err)
+				c.JSON(http.StatusBadRequest, gin.H{
+					"message": err.Error(),
+				})
+			} else {
+				c.Data(http.StatusOK, gin.MIMEJSON, []byte(resp[0]))
+			}
+		}
+		logInfo(c)
+	})
+
+	a.POST("input/:value", func(c *gin.Context) {
+		value := c.Params.ByName("value")
+		if value == "" {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "empty value was given",
+			})
+		} else {
+			input <- structures.Request{
+				KeyFamily: "ram_usage",
+				Key:       time.Now().Format("2006:01:02T15:04"),
+				Value:     value,
+				Action:    "set",
+			}
+			resp, err := db.HandleOutput(output, timeout)
+			if err != nil {
+				c.JSON(http.StatusBadRequest, gin.H{
+					"message": err.Error(),
+				})
+			} else {
+				c.JSON(http.StatusOK, gin.H{
+					"message": resp,
+				})
+			}
+		}
+		logInfo(c)
+	})
+
+	r.Run()
+}
